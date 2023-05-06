e9b28768e78da9100201f499daa33d116361fce9
jonathan_dreyer <jonathan.dreyer60@gmail.com>
2023-05-06 14:59:02 +0200

    crud-with-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+
+puts "Creating some amazing seeds"
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    rating: Faker::Number.between(from: 1, to: 5),
+    address: Faker::Address.full_address
+  )
+end
+puts "5 were created"
